da248a947cd9a1656f5527c10d011677169b50ed
FireStreaker2 <michaelccc2019@outlook.com>
2023-09-29 16:56:06 -0700

    image fetching

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ body.style.alignItems = "center";
 body.style.justifyContent = "space-around";
 body.style.fontFamily = "Arial";
 
+// remove this (development purposes)
+body.style.position = "absolute";
+body.style.left = "50%";
+body.style.top = "50%";
+
 const text = document.createElement("p");
 text.textContent = "I'm not a robot";
 
@@ -35,15 +40,55 @@ const title = document.createElement("p");
 const imageContainer = document.createElement("div");
 imageContainer.style.width = "100%";
 imageContainer.style.height = "70%";
+imageContainer.style.display = "flex";
+imageContainer.style.flexDirection = "row";
+imageContainer.style.flexWrap = "wrap";
 
 const button = document.createElement("button");
 button.style.width = "2rem";
 button.style.height = "2rem";
 
 button.addEventListener("click", () => {
-  captchaBody.style.display == "none" ? captchaBody.style.display = "flex" : captchaBody.style.display = "none";
-});
+	captchaBody.style.display == "none"
+		? (captchaBody.style.display = "flex")
+		: (captchaBody.style.display = "none");
 
+	if (captchaBody.style.display == "none") {
+		return;
+	}
+
+	const indicators = ["felix_argyle", "hololive"];
+	let images = [];
+
+	const indicator = indicators[Math.floor(Math.random() * indicators.length)];
+
+	for (let i = 0; i < 9; i++) {
+		fetch(`https://femboyfinder.firestreaker2.gq/api/${indicator}`)
+			.then((response) => response.json())
+			.then((data) => {
+				images.push(data.URL);
+				console.log(data.URL);
+
+				const image = document.createElement("img");
+				image.style.width = "100%";
+				image.style.height = "auto";
+				image.src = data.URL;
+
+				const container = document.createElement("a");
+				container.style.overflow = "hidden";
+				container.style.display = "flex";
+				container.style.width = "6rem";
+				container.style.height = "6rem";
+				container.appendChild(image);
+
+				imageContainer.appendChild(container);
+			})
+			.catch((error) => {
+				captchaBody.style.display = "none";
+				console.error(`[ERROR] ${error}`);
+			});
+	}
+});
 
 titleContainer.appendChild(title);
 captchaBody.appendChild(titleContainer);
