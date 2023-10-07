68278304b768d02e71b2dcdb5083322e3b1cdc8b
FireStreaker2 <michaelccc2019@outlook.com>
2023-10-07 10:16:00 -0700

    functionality

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ body.style.left = "50%";
 body.style.top = "50%";
 //
 
+let selected = [];
+let correctImages = [];
+
 const text = document.createElement("p");
 text.textContent = "I'm not a robot";
 
@@ -57,7 +60,7 @@ const button = document.createElement("button");
 button.style.width = "2rem";
 button.style.height = "2rem";
 
-const append = (url) => {
+const append = (url, type) => {
 	const image = document.createElement("img");
 	image.style.width = "100%";
 	image.style.height = "auto";
@@ -68,7 +71,35 @@ const append = (url) => {
 	container.style.display = "flex";
 	container.style.width = "6rem";
 	container.style.height = "6rem";
+	container.style.cursor = "pointer";
+	container.setAttribute("waifu-clicked", "false");
 	container.appendChild(image);
+
+	if (type === "correct") {
+		container.addEventListener("click", () => {
+			const data = { image: url, type: "correct" };
+
+			if (container.getAttribute("waifu-clicked") === "false") {
+				selected.push(data);
+				container.setAttribute("waifu-clicked", "true");
+			} else {
+				selected.splice(selected.indexOf(data), 1);
+				container.setAttribute("waifu-clicked", "false");
+			}
+		});
+	} else {
+		container.addEventListener("click", () => {
+			const data = { image: url, type: "incorrect" };
+
+			if (container.getAttribute("waifu-clicked") === "false") {
+				selected.push(data);
+				container.setAttribute("waifu-clicked", "true");
+			} else {
+				selected.splice(selected.indexOf(data), 1);
+				container.setAttribute("waifu-clicked", "false");
+			}
+		});
+	}
 
 	imageContainer.appendChild(container);
 };
@@ -80,11 +111,11 @@ button.addEventListener("click", () => {
 
 	if (captchaBody.style.display == "none") {
 		imageContainer.innerHTML = "";
+		selected = [];
 		return;
 	}
 
 	const indicators = ["felix_argyle", "hoshino_ruby", "hoshino_ai"];
-	let images = [];
 
 	const primaryIndicator =
 		indicators[Math.floor(Math.random() * indicators.length)];
@@ -105,9 +136,12 @@ button.addEventListener("click", () => {
 		fetch(`https://femboyfinder.firestreaker2.gq/api/${primaryIndicator}`)
 			.then((response) => response.json())
 			.then((data) => {
-				images.push(data.URL);
-
-				i === amount ? (exampleImage.src = data.URL) : append(data.URL);
+				if (i === amount) {
+					exampleImage.src = data.URL;
+				} else {
+					correctImages.push(data.URL);
+					append(data.URL, "correct");
+				}
 			})
 			.catch((error) => {
 				captchaBody.style.display = "none";
@@ -119,8 +153,7 @@ button.addEventListener("click", () => {
 		fetch(`https://femboyfinder.firestreaker2.gq/api/${secondaryIndicator}`)
 			.then((response) => response.json())
 			.then((data) => {
-				images.push(data.URL);
-				append(data.URL);
+				append(data.URL, "incorrect");
 			})
 			.catch((error) => {
 				captchaBody.style.display = "none";
@@ -129,10 +162,54 @@ button.addEventListener("click", () => {
 	}
 });
 
+const submitButton = document.createElement("button");
+submitButton.style.width = "6rem";
+submitButton.style.height = "2rem";
+submitButton.style.position = "absolute";
+submitButton.style.right = "0";
+submitButton.style.bottom = "0";
+submitButton.style.margin = "0.5rem";
+submitButton.textContent = "Submit";
+
+submitButton.addEventListener("click", () => {
+	const correctSelectedUrls = selected
+		.filter((item) => item.type === "correct")
+		.map((item) => item.image);
+
+	const incorrectSelectedUrls = selected
+		.filter((item) => item.type === "incorrect")
+		.map((item) => item.image);
+
+	if (incorrectSelectedUrls.length !== 0) {
+		alert("Wrong");
+		captchaBody.style.display = "none";
+		imageContainer.innerHTML = "";
+		selected = [];
+		correctImages = [];
+    return;
+	}
+
+	for (let i = 1; i <= correctSelectedUrls.length; i++) {
+		correctImages.splice(correctImages.indexOf(correctSelectedUrls[i]), 1);
+	}
+
+	if (correctImages.length === 0) {
+		alert("Passed");
+	} else {
+		alert("Wrong");
+	}
+
+	captchaBody.style.display = "none";
+	imageContainer.innerHTML = "";
+	selected = [];
+	correctImages = [];
+});
+
 titleContainer.appendChild(titleText);
 titleContainer.appendChild(exampleImage);
 captchaBody.appendChild(titleContainer);
 captchaBody.appendChild(imageContainer);
+captchaBody.appendChild(submitButton);
 body.appendChild(button);
 body.appendChild(text);
 body.appendChild(image);
